2838daf55a8b5b9039d2775c8239731efd5b8004
kiruwi <i4nkirui@gmail.com>
2025-05-25 16:55:50 +0300

    Update Resume links to download the PDF file instead of opening in new tab

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -38,7 +38,7 @@ const Sidebar=({isOpen, toggle, currentTheme, toggleTheme}) => {
       <SidebarWrapper>
         <SidebarMenu>
           <SidebarLink to='services' onClick={toggle}>Portfolio</SidebarLink>
-          <SidebarLink as="a" href='/assets/cv/iancheruiyot design cv.pdf' target="_blank" rel="noopener noreferrer" onClick={toggle} style={{ textDecoration: 'none' }}>Resume</SidebarLink>
+          <SidebarLink as="a" href='/assets/cv/iancheruiyot design cv.pdf' download="Ian_Cheruiyot_CV.pdf" rel="noopener noreferrer" onClick={toggle} style={{ textDecoration: 'none' }}>Resume</SidebarLink>
           <SidebarLink as={Link} to='/ContactUs' onClick={toggle} style={{ textDecoration: 'none' }}>Contact Me</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
